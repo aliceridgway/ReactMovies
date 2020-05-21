0fbaa8c41cc67cdf1eb30563fec4cdeb50f1c440
Alice Campkin <alice.campkin@gmail.com>
2020-05-21 16:40:08 +0100

    Updates

---
+++ precache-manifest.d3dba7c9ed6789da618ec5dba0c5ba9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21bd403e207d58eef86dcec95951835c",
+    "url": "/ReactMovies/index.html"
+  },
+  {
+    "revision": "242ec571a62bcd1d5dba",
+    "url": "/ReactMovies/static/css/main.b3fabd2b.chunk.css"
+  },
+  {
+    "revision": "f285a6867a8060145090",
+    "url": "/ReactMovies/static/js/2.595edd1a.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/ReactMovies/static/js/2.595edd1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "242ec571a62bcd1d5dba",
+    "url": "/ReactMovies/static/js/main.ff9921ee.chunk.js"
+  },
+  {
+    "revision": "8dbfb86501299628a7b8",
+    "url": "/ReactMovies/static/js/runtime-main.65d10add.js"
+  }
+]);
