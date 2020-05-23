d21817d8a28b4f221a8c0260973e04ead87b54c7
Alice Campkin <alice.campkin@gmail.com>
2020-05-23 16:52:40 +0100

    refactored into functional component with hooks. Placeholders for images added

--- src/components/Moviedetail.js
+++ src/components/Moviedetail.js
@@ -1,55 +1,73 @@
-/* eslint-disable react/destructuring-assignment */
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Overdrive from 'react-overdrive';
+import PropTypes from 'prop-types';
 import { Poster } from './movie';
+
+import placeholderBackdrop from '../placeholder_backdrop_w1280.jpg';
+import placeholderPoster from '../placeholder_poster_w154.jpg';
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280';
 
-class MovieDetail extends Component {
-  state = {
-    movie: {},
-  }
+const MovieDetail = ({ match }) => {
+  const [movie, setMovie] = useState({});
+  const [backdrop, setBackdrop] = useState(placeholderBackdrop);
+  const [poster, setPoster] = useState(placeholderPoster);
 
-  async componentDidMount() {
-    try {
-      const res = await fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=49351c46224cf6108890d28e7e5f0d50&language=en-US`);
-      const movie = await res.json();
+  useEffect(() => {
+    const fetchMovie = async () => {
+      try {
+        const res = await fetch(`https://api.themoviedb.org/3/movie/${match.params.id}?api_key=49351c46224cf6108890d28e7e5f0d50&language=en-US`);
+        const result = await res.json();
+        setMovie(result);
+        setBackdrop(BACKDROP_PATH + result.backdrop_path);
+        setPoster(POSTER_PATH + result.poster_path);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchMovie();
+  }, []);
 
-      this.setState({
-        movie,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  return (
 
-  render() {
-    const { movie } = this.state;
-    return (
-      <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
+    <MovieWrapper backdrop={backdrop}>
+      <MovieInfo>
 
-        <MovieInfo>
+        <Overdrive id={match.params.id}>
+          <Poster src={poster} alt={movie.title} />
+        </Overdrive>
 
-          <Overdrive id={movie.id}>
-            <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-          </Overdrive>
+        <div>
+          <h1>{movie.title}</h1>
+          <h3>{movie.release_date}</h3>
+          <p>{movie.overview}</p>
+        </div>
 
-          <div>
-            <h1>{movie.title}</h1>
-            <h3>{movie.release_date}</h3>
-            <p>{movie.overview}</p>
-          </div>
+      </MovieInfo>
 
-        </MovieInfo>
-
-      </MovieWrapper>
-    );
-  }
-}
+    </MovieWrapper>
+  );
+};
 
 export default MovieDetail;
+
+MovieDetail.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+};
+
+MovieDetail.defaultProps = {
+  match: {
+    params: {
+      id: 1,
+    },
+  },
+};
 
 const MovieWrapper = styled.div`
     position:relative;
@@ -63,6 +81,7 @@ const MovieInfo = styled.div`
     text-align:left;
     padding:2rem 10%;
     display:flex;
+    color:black;
 
     > div{
         margin-left:4rem;
