1fbdec7d1a8bb09a4e873e16b71570650bebc3f9
Alice Campkin <alice.campkin@gmail.com>
2020-05-23 13:50:21 +0100

    refactored to use hooks inside stateless functional component

--- src/components/Movieslist.js
+++ src/components/Movieslist.js
@@ -1,42 +1,52 @@
-/* eslint-disable react/destructuring-assignment */
-import React, { Component } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import Movie from './movie';
 
-class Movieslist extends Component {
-  state = {
-    movies: [],
-  }
 
-  async componentDidMount() {
-    try {
+const MoviesList = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
       const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=49351c46224cf6108890d28e7e5f0d50&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
-      const movies = await res.json();
-      console.log(movies);
+      const moviesList = await res.json();
+      setMovies(moviesList.results);
+    };
+    fetchMovies();
+  }, []);
 
-      this.setState({
-        movies: movies.results,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
-  render() {
-    return (
-      <MovieGrid>
-        {this.state.movies.map((movie) => (<Movie key={movie.id} movie={movie} />))}
-      </MovieGrid>
-    );
-  }
-}
+  return (
+    <MovieGrid>
+      {movies.map((movie) => (<Movie key={movie.id} movie={movie} />))}
+    </MovieGrid>
+  );
+};
 
-export default Movieslist;
+export default MoviesList;
+
+MoviesList.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      page: PropTypes.number,
+    }),
+  }),
+};
+
+MoviesList.defaultProps = {
+  match: {
+    params: {
+      page: 1,
+    },
+  },
+};
 
 const MovieGrid = styled.div` 
   display:grid;
   padding:1rem;
-  grid-template-columns:repeat(6,1fr);
+  grid-template-columns:repeat(5,auto);
   grid-row-gap:1rem;
 
 `;
