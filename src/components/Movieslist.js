525c0d5fafe6ec508218f344882b5098c0794a96
Alice Campkin <alice.campkin@gmail.com>
2020-05-23 16:54:31 +0100

    propTypes added

--- src/components/Movieslist.js
+++ src/components/Movieslist.js
@@ -4,28 +4,39 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import Movie from './movie';
 
+import Pagination from './pagination';
 
 const MoviesList = () => {
   const [movies, setMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
+    console.log('useEffect');
     const fetchMovies = async () => {
-      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=49351c46224cf6108890d28e7e5f0d50&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
-      const moviesList = await res.json();
-      setMovies(moviesList.results);
+      const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=49351c46224cf6108890d28e7e5f0d50&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${currentPage}`);
+      const response = await res.json();
+      console.log(response);
+      setMovies(response.results);
+      setTotalPages(response.total_pages);
     };
     fetchMovies();
-  }, []);
+  }, [currentPage]);
 
+  const paginate = (page) => setCurrentPage(page);
 
   return (
-    <MovieGrid>
-      {movies.map((movie) => (<Movie key={movie.id} movie={movie} />))}
-    </MovieGrid>
+    <>
+      <MovieGrid>
+        {movies.map((movie) => (<Movie key={movie.id} movie={movie} />))}
+      </MovieGrid>
+      <Pagination currentPage={currentPage} paginate={paginate} totalPages={totalPages} />
+    </>
   );
 };
 
 export default MoviesList;
+
 
 MoviesList.propTypes = {
   match: PropTypes.shape({
