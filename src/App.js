e7ce162934383ce7152057716a491f0b1851cea2
Alice Campkin <alice.campkin@gmail.com>
2020-05-26 21:47:34 +0100

    Layout component added

--- src/App.js
+++ src/App.js
@@ -1,55 +1,23 @@
-/* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import styled from 'styled-components';
 import {
   HashRouter as Router,
   Route,
   Switch,
-  Link,
 } from 'react-router-dom';
-
-import './App.css';
 
 import MoviesList from './components/MoviesList';
 import Moviedetail from './components/Moviedetail';
-
+import Layout from './components/Layout';
 
 const App = () => (
-
   <Router basename="/">
-    <div className="App">
-
-      <AppHeader>
-        <Link to="/">
-          <h1>movies</h1>
-        </Link>
-      </AppHeader>
-
+    <Layout>
       <Switch>
         <Route exact path="/" component={MoviesList} />
         <Route path="/:id" component={Moviedetail} />
       </Switch>
-
-    </div>
+    </Layout>
   </Router>
-
 );
 
 export default App;
-
-const AppHeader = styled.header`
-  background-color: #111;
-  height:60px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: #EEE;
-
-  a{
-    color:inherit;
-    text-decoration:none;
-    font-size:1rem;
-  }
-`;
