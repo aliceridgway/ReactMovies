a39ca1e8eb37eb91cc217d046ac3ba40a59c545b
Alice Campkin <alice.campkin@gmail.com>
2020-05-26 21:50:39 +0100

    Popular movies slider added. Layout changed

--- src/components/Movieslist.js
+++ src/components/Movieslist.js
@@ -2,9 +2,13 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import Movie from './movie';
+import Inner from './Inner';
+import Card from './Card';
+import Logo from './Logo';
+import PopularMovies from './PopularMovies';
 
 import Pagination from './pagination';
+import background from '../images/background_w1980.jpg';
 
 const MoviesList = () => {
   const [movies, setMovies] = useState([]);
@@ -14,7 +18,7 @@ const MoviesList = () => {
   useEffect(() => {
     console.log('useEffect');
     const fetchMovies = async () => {
-      const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=49351c46224cf6108890d28e7e5f0d50&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${currentPage}`);
+      const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=49351c46224cf6108890d28e7e5f0d50&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${currentPage + 1}`);
       const response = await res.json();
       console.log(response);
       setMovies(response.results);
@@ -27,10 +31,24 @@ const MoviesList = () => {
 
   return (
     <>
-      <MovieGrid>
-        {movies.map((movie) => (<Movie key={movie.id} movie={movie} />))}
-      </MovieGrid>
-      <Pagination currentPage={currentPage} paginate={paginate} totalPages={totalPages} />
+      <Background src={background} />
+
+      <Logo />
+      {(currentPage === 1) && (
+      <PopularMovies APIString="https://api.themoviedb.org/3/discover/movie?api_key=49351c46224cf6108890d28e7e5f0d50&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1">
+        <h2>Trending Movies</h2>
+      </PopularMovies>
+      ) }
+
+
+      <Inner>
+        {(currentPage === 1) && <h2>More great movies...</h2>}
+        <MovieGrid>
+          {movies.map((movie) => (<Card key={movie.id} movie={movie} />))}
+        </MovieGrid>
+        <Pagination currentPage={currentPage} paginate={paginate} totalPages={totalPages} />
+      </Inner>
+
     </>
   );
 };
@@ -55,9 +73,19 @@ MoviesList.defaultProps = {
 };
 
 const MovieGrid = styled.div` 
-  display:grid;
-  padding:1rem;
-  grid-template-columns:repeat(5,auto);
-  grid-row-gap:1rem;
+  display:flex;
+  flex-wrap:wrap;
+  
+  >div{
+    margin: 0rem 1rem 1rem 0;
+  }
 
+`;
+const Background = styled.img`
+  position:absolute;
+  top:0;
+  left:0;
+  width:100%;
+  object-fit:contain;
+  z-index:-1;
 `;
